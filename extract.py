f5e653ea4b70c036fef72a26d9624954e3be3861
tltang <tltang@yahoo.com>
2022-12-02 13:50:39 -0500

    initial checkin

---
+++ extract.py
@@ -0,0 +1,57 @@
+"""Extract data on near-Earth objects and close approaches from CSV and JSON files.
+
+The `load_neos` function extracts NEO data from a CSV file, formatted as
+described in the project instructions, into a collection of `NearEarthObject`s.
+
+The `load_approaches` function extracts close approach data from a JSON file,
+formatted as described in the project instructions, into a collection of
+`CloseApproach` objects.
+
+The main module calls these functions with the arguments provided at the command
+line, and uses the resulting collections to build an `NEODatabase`.
+
+You'll edit this file in Task 2.
+"""
+import csv
+import json
+
+from models import NearEarthObject, CloseApproach
+
+def load_neos(neo_csv_path):
+    """Read near-Earth object information from a CSV file.
+
+    :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
+    :return: A collection of `NearEarthObject`s.
+    """
+    List_NEO = []
+
+    #3 pdes,4 name,7 pha,15 diameter
+
+    with open(neo_csv_path, 'r') as infile:
+        contents = csv.reader(infile)
+        next(contents)  # skip header
+        for row in contents:
+            neo = NearEarthObject(designation=row[3], name=row[4], diameter=row[15], hazardous=row[7])
+            List_NEO.append(neo)
+    return List_NEO
+
+def load_approaches(cad_json_path):
+    """Read close approach data from a JSON file.
+
+    :param cad_json_path: A path to a JSON file containing data about close approaches.
+    :return: A collection of `CloseApproach`es.
+    """
+    cad = []
+
+    with open(cad_json_path, 'r') as infile:
+        contents = json.load(infile)  # Parse JSON data into a Python object. (A)
+
+    for item in contents['data']:
+        ca = CloseApproach(designation=item[0], time=item[3], distance=item[4], velocity=item[7])
+        cad.append(ca)
+
+    return cad
+
+# if __name__ == "__main__":
+#     load_neos('tests/test-neos-2020.csv')
+#     load_approaches('tests/test-cad-2020.json')
